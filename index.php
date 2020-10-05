2522ee4cf2554dbe3fc89d7903a063eaaf112fda
farajullah94 <farajullah@icloud.com>
2020-10-05 11:34:45 +0800

    Menambahkan list

--- index.php
+++ index.php
@@ -11,6 +11,14 @@
     <h1>Hello World</h1>
 
     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Enim, a, numquam molestiae officia exercitationem minus quod quibusdam aliquid porro itaque temporibus. Possimus maiores laudantium ut cupiditate quasi autem est quae.</p>
+
+    <ol>
+        <li>List 1</li>
+        <li>List 2</li>
+        <li>List 3</li>
+        <li>List 4</li>
+        <li>List 5</li>
+    </ol>
 </body>
 
 </html>
